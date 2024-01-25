ad40e2fe6dc3e41bc1727337f6db03fd585cc430
Michael Yan <rain@rainboyan.com>
2024-01-25 21:27:16 +0800

    Update Application.groovy

--- json/grails-app/init/grails/plugin/json/view/Application.groovy
+++ json/grails-app/init/grails/plugin/json/view/Application.groovy
@@ -1,7 +1,11 @@
 package grails.plugin.json.view
 
 import grails.boot.Grails
+import grails.plugins.metadata.PluginSource
+import groovy.transform.CompileStatic
 
+@PluginSource
+@CompileStatic
 class Application {
     static void main(String[] args) {
         Grails.run(Application, args)
